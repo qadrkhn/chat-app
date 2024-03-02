d6e5b807652933447c9b5c314182031e8d0533e6
Qadeer Khan <qadeer@horizondigital.net.au>
2024-03-03 01:16:10 +0500

    Feat: New templates and configured static files

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('main', Main.as_view(), name = 'main-view'),
+    path('', Main.as_view(), name = 'main-view'),
     path('home', Home.as_view(), name = 'home-view'),
     path('chat_person', ChatPerson.as_view(), name = 'chat-person'),
 ]
